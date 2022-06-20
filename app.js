3ab3fdf4fa9b3f8e3f26e2d8084810f50cdc3794
sayanij7 <sayanij76@gmail.com>
2022-06-20 23:35:18 +0530

    add date.js module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -14,16 +15,7 @@ app.set('view engine', 'ejs');
 
 //load the home route
 app.get("/", function (req, res) {
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-IN", options);
-
-
+    let day = date();
     res.render('list', { listType: day, newlyAddedItems: items });
 });
 
